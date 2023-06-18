45a1bdb87b2a298ba7e851536964abd88d423a47
Aditya Kumar Singh <60130665+DashingAdi@users.noreply.github.com>
2023-06-18 13:44:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const now = new Date();
 const day = date.format(now, 'DD');
 const month = date.format(now, 'MM');
 const D_day = [18,8];
-const temp = Number(D_day[1]) - month
-const timeAwait = temp*30 + D_day[0] - day
-require("dotenv").config();
+const temp = Number(D_day[1]) - month;
+const timeAwait = temp*30 + D_day[0] - day;
 const token = process.env.BOT_TOKEN;
 
 const bot = new Telegram(token, {polling:true});
